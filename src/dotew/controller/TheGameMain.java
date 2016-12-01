217c304ebdee92ef0f5527f51adb38cbfa57a5d3
Utku Oymak <utkuskyrim@gmail.com>
2016-12-02 00:21:29 +0300

    Main try
    M

--- src/dotew/controller/TheGameMain.java
+++ src/dotew/controller/TheGameMain.java
@@ -3,7 +3,7 @@ package dotew.controller;
 public class TheGameMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello dotew!!");
 
 	}
 
