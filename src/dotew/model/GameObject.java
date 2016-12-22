f950beec9e8c53066116236324856a0dce1fe5b0
ilteber <ayvaci.ilteber@gmail.com>
2016-12-22 04:52:23 +0300

    gameObject

--- src/dotew/model/GameObject.java
+++ src/dotew/model/GameObject.java
@@ -1,5 +1,19 @@
 package dotew.model;
-
+import java.awt.Image;
 public class GameObject {
-
+	private String name;
+	private Image Icon;
+	private TerrainCell tCell;
+	private boolean interactiveness;
+	public GameObject() {
+		// TODO Auto-generated constructor stub
+	}
+	public boolean move(TerrainCell t){
+		
+		if(t!= null){
+			this.tCell = t;
+			return true;
+		}
+		return false;
+	}
 }
