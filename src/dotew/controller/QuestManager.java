403b17f28b795336ba14e021830e85cd603c33a2
afradmk <afra.domeke@ug.bilkent.edu.tr>
2016-12-24 00:39:43 +0300

    quest

--- src/dotew/controller/QuestManager.java
+++ src/dotew/controller/QuestManager.java
@@ -1,5 +1,38 @@
 package dotew.controller;
+import dotew.model.Quest;
+import dotew.model.Player;
 
 public class QuestManager {
 
+	private int totalQuests;
+	private int totalQuestsFinished;
+	private Quest currentQuest;
+	private Quest[] ongoingQuests;
+	
+	public QuestManager(int totalQuests,  int totalQuestsFinished, Quest currentQuest,	Quest[] ongoingQuests){
+		this.totalQuests = totalQuests;
+		this.totalQuestsFinished = totalQuestsFinished;
+		this.currentQuest = null;
+		this.ongoingQuests = null;
+		
+	}
+	
+	public void switchCurrentQuests(Player p){
+		//
+	}
+	
+	public void updateCurrentQuest(Player p){
+		
+	}
+	
+	public boolean checkQuestEnding(Player p){
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
