780626ba1f1e37a97e398da5a5c06e9e8db47d7a
Tugberk Topallar <sarptugberk@gmail.com>
2016-12-24 01:44:39 +0300

    map move

--- src/dotew/controller/MapManager.java
+++ src/dotew/controller/MapManager.java
@@ -1,5 +1,90 @@
 package dotew.controller;
 
+import dotew.model.CCharacter;
+import dotew.model.Map;
+import dotew.model.TerrainCell;
+
 public class MapManager {
-//TODO: 
+	//TODO:
+	private Map currentMap;
+	
+	//getset
+	public Map getCurrentMap(){
+		return currentMap;
+	}
+	public void setCurrentMap(Map currentMap){
+		this.currentMap = currentMap;
+	}
+	
+	public void loadMap(){
+		
+	}
+	
+	public void createMonsters(){
+		
+	}
+	
+	public void createPlayer(){
+		
+	}
+	
+	public void createNPCandTerrain(){
+		
+	}
+	
+	public void move(String direction, CCharacter player){
+		int currentX=0, currentY=0, destinationX=0, destinationY=0;
+		//find current coordinates
+		for(int i =0; i<currentMap.getTerrainCellList().length; i++){
+			for(int j =0; j<currentMap.getTerrainCellList()[i].length; j++){
+				if(currentMap.getTerrainCellList()[i][j].getGameObject() == player){
+					currentX = i;
+					currentY= j;
+				}
+			}
+		}
+		//check possible 4 destinations
+		if(direction.equalsIgnoreCase("north")){
+			destinationX = currentX-1;
+			destinationY = currentY;
+		}
+		
+		if(direction.equalsIgnoreCase("west")){
+			destinationX = currentX;
+			destinationY = currentY+1;
+		}
+		
+		if(direction.equalsIgnoreCase("south")){
+			destinationX = currentX+1;
+			destinationY = currentY;
+		}
+		
+		if(direction.equalsIgnoreCase("east")){
+			destinationX = currentX;
+			destinationY = currentY-1;
+		}
+		
+		//boundary check only works for N*N map configuration!
+		if(destinationX <0 || destinationX > currentMap.getTerrainCellList()[0].length || destinationY <0 || destinationY>currentMap.getTerrainCellList()[0].length){
+			//you cannot go there
+		}else{
+			if(CollisionHandler.checkForCollision(player, currentMap.getTerrainCellList()[destinationX][destinationY])){
+				//collisionHandler handles the collision
+			}else{
+				player.move(currentMap.getTerrainCellList()[destinationX][destinationY]);
+			}
+		}	
+	}
+	
+	public void spawnMonsters(){
+		
+	}
+	
+	public void drawMap(){
+		
+	}
+	
+	public void startBattle(){
+		
+	}
 }
