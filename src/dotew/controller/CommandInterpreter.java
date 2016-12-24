0fdb752f6c2fbeea82fe29f7446aaee094fa0c4a
afradmk <afra.domeke@ug.bilkent.edu.tr>
2016-12-24 04:20:26 +0300

    skill kısmı tamam

--- src/dotew/controller/CommandInterpreter.java
+++ src/dotew/controller/CommandInterpreter.java
@@ -1,14 +1,12 @@
 package dotew.controller;
 
 import java.util.ArrayList;
-
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import dotew.model.CCharacter;
 
 import dotew.model.NPC;
-
+import dotew.model.*;
 import dotew.model.Skill;
 
 import java.io.IOException;
@@ -44,6 +42,7 @@ public class CommandInterpreter {
 	public void checkSyntax(String command){
 		
 	String chName = "";
+	String[] text ;
 	command = command.toLowerCase();
 
 	
@@ -57,7 +56,13 @@ public class CommandInterpreter {
 		//applyskill method, determines enemy index, skill name
 		if(command.substring(0 , 10).equals("applyskill")){
 			text = command.split(" ");
-			return matchCommand("applyskill", game.getBattleManager().getCharacterList().indexOf(text[1]), text[2]);
+			String c2name = text[1];
+			String skillName = "";
+			for(int i = 2; i< text.length; i++){
+				skillName = skillName+text[i];
+			}
+			
+			matchCommand("applyskill", game.getBattleManager().getCharacterList().indexOf(c2name), skillName);
 		}	
 			
 		//for other methods
@@ -88,19 +93,77 @@ public class CommandInterpreter {
 				    	
 				    //use skill(skill name, on ch)
 					case 6:
-						switch(skill){
+					   switch(skill){
 						
+					   //mages
 						case "fireball":
-							game.
+							Mage1Skill m1 = new Mage1Skill();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), m1);
+							break;
+							
+						case "icespike":
+							Mage2Skill m2 = new Mage2Skill();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), m2);
+							break;
+							
+						case "deflect":
+							Mage3Skill m3 = new Mage3Skill();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), m3);
+							break;
+							
+						case "cosmiclight":
+							Mage4Skill1 m4 = new Mage4Skill1();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), m4);
+							break;
+							
+						//rogues	
+						case "invisibility":
+							Rogue4Skill r4 = new Rogue4Skill();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), r4);
+							break;
+							
+						case "knifethrow":
+							Rogue3Skill r3 = new Rogue3Skill();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), r3);
+							break;
 						
-						}
+						case "pisonknife":
+							Rogue2Skill r2 = new Rogue2Skill();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), r2);
+							break;
+	
+						case "backstab":
+							Rogue1Skill r1 = new Rogue1Skill();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), r1);
+							break;
+							
+						//warriors
+						case "chargee!!":
+							Warrior1Skill w1 = new Warrior1Skill();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), w1);
+							break;
 						
-						//game.getBattleManager().applySkill(game.getPlayer(),  (CCharacter) game.getBattleManager().getCharacterList().get(index), );
+						case "ravage":
+							Warrior2Skill w2 = new Warrior2Skill();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), w2);
+							break;
+						
+						case "block":
+							Warrior3Skill w3 = new Warrior3Skill();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), w3);
+							break;
+						
+						case "berserker'sblood":
+							Warrior4Skill w4 = new Warrior4Skill();
+							game.getPlayer().useSkill((CCharacter) game.getBattleManager().getCharacterList().get(index), w4);
+							break;
+													
+						}						
 						break;
 						
 					//is there anything i can help you with
 					case 7:
-						npc.giveQuest(game.getPlayer());
+						//npc give quest
 						break;
 						   
 				}
